58d783bd19ad25171e15e5e7baf488b23bb1fa98
Larsa-mrp <bradleyspiclin@gmail.com>
2023-06-06 20:18:20 +1000

    configure CORS

--- Program.cs
+++ Program.cs
@@ -12,49 +12,37 @@ namespace TaskPlannerApi
 
             // Add services to the container.
             builder.Services.AddControllers();
-
             var connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+
             // DbContext configuration
             builder.Services.AddDbContext<TaskContext>(options => options.UseSqlServer(connection));
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(builder =>
-                {
-                    builder.WithOrigins("http://localhost:3000")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
-            });
+            builder.Services.AddSwaggerGen();                        
+            builder.Services.AddCors();
 
             var app = builder.Build();
-            app.UseCors();
 
             app.UseRouting();
 
-            // Other middleware
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin() // Allow any origin
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
 
             app.UseAuthorization();
 
-
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
             app.UseHttpsRedirection();
-
-
-
-            app.MapControllers();
-
             app.Run();
         }
     }
