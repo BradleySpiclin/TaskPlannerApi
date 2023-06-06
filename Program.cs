d4515130d9ca2e3ac22ab3d2a869acba49eba8cf
Larsa-mrp <bradleyspiclin@gmail.com>
2023-06-06 19:35:34 +1000

    Added cors

--- Program.cs
+++ Program.cs
@@ -18,8 +18,13 @@ namespace TaskPlannerApi
             builder.Services.AddDbContext<TaskContext>(options => options.UseSqlServer(connection));
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
 
             var app = builder.Build();
+            app.UseCors(builder => builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
